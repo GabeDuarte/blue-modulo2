c7ba804c3420c7a804f508714f8534528b2e27d8
GabeDuarte <83724430+GabeDuarte@users.noreply.github.com>
2021-07-23 11:27:34 -0300

    att

--- módulo 3/react/01/src/App.js
+++ módulo 3/react/01/src/App.js
@@ -15,6 +15,13 @@ export default class App extends React.Component {
           qtd_temp: "12",
         },
         {
+          nome: "gabriel duarte",
+          imagem_url:
+            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
+          ano_lancamento: "2007",
+          qtd_temp: "12",
+        },
+        {
           nome: "Friends",
           imagem_url: "https://br.web.img3.acsta.net/pictures/19/12/20/21/27/3055482.jpg",
           ano_lancamento: "1994",
@@ -32,11 +39,49 @@ export default class App extends React.Component {
           ano_lancamento: "2008",
           qtd_temp: "5",
         },
+        {
+          nome: "Breaking Bad",
+          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
+          ano_lancamento: "2008",
+          qtd_temp: "5",
+        },
+        {
+          nome: "Breaking Bad",
+          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
+          ano_lancamento: "2008",
+          qtd_temp: "5",
+        },
+        {
+          nome: "Breaking Bad",
+          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
+          ano_lancamento: "2008",
+          qtd_temp: "5",
+        },
+        {
+          nome: "Breaking Bad",
+          imagem_url: "https://presleyson.com.br/wp-content/uploads/2018/12/breaking-bad-800x800-min.png?a7c535&a7c535",
+          ano_lancamento: "2008",
+          qtd_temp: "5",
+        },
+        {
+          nome: "The Big Bang Theory",
+          imagem_url:
+            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
+          ano_lancamento: "2007",
+          qtd_temp: "12",
+        },
+        {
+          nome: "The Big Bang Theory",
+          imagem_url:
+            "https://s2.glbimg.com/476xiyzHwobzomhiO6QZ8ZaZPCM=/362x536/https://s2.glbimg.com/gw3G9SRdfhWZbNxnMuwbznGiAW0=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2020/s/Y/z9FwyXQjq1flWRIm3mUA/2020-726-series-warner-the-big-bang-theory-poster.jpg",
+          ano_lancamento: "2007",
+          qtd_temp: "12",
+        },
       ],
-      nome:'',
-      imagem_url:'',
-      ano_lancamento:'',
-      qtd_temp:'',
+      nomeSerie:'',
+      imagem_urlSerie:'',
+      ano_lancamentoSerie:'',
+      qtd_tempSerie:'',
       editando: false,
       indexEditando: null,
     };
@@ -45,15 +90,15 @@ export default class App extends React.Component {
   onSubmit= (e) =>{
     e.preventDefault();
 
-    const {series, editando, indexEditando, nome,  imagem_url, qtd_temp, ano_lancamento } = this.state;
+    const {series, editando, indexEditando, nomeSerie,  imagem_urlSerie, qtd_tempSerie, ano_lancamentoSerie } = this.state;
 
     if (editando){
       const seriesAtualizadas = series.map((serie, index) => {
         if (indexEditando === index){
-          serie.nome = nome;
-          serie.ano_lancamento = ano_lancamento;
-          serie.imagem_url = imagem_url;
-          serie.qtd_temp = qtd_temp;
+          serie.nome = nomeSerie;
+          serie.ano_lancamento = ano_lancamentoSerie;
+          serie.imagem_url = imagem_urlSerie;
+          serie.qtd_temp = qtd_tempSerie;
 
         }
         return serie;
@@ -70,19 +115,19 @@ export default class App extends React.Component {
         series: [
           ...series,
           {
-            nome: nome,
-            ano_lancamento: ano_lancamento,
-            imagem_url: imagem_url,
-            qtd_temp: qtd_temp
+            nome: nomeSerie,
+            ano_lancamento: ano_lancamentoSerie,
+            imagem_url: imagem_urlSerie,
+            qtd_temp: qtd_tempSerie
           },
         ]
       });
     };
     this.setState({
-      nome: '',
-      imagem_url:'',
-      qtd_temp: '',
-      ano_lancamento:''
+      nomeSerie: '',
+      imagem_urlSerie:'',
+      qtd_tempSerie: '',
+      ano_lancamentoSerie:''
     });
   };
 
@@ -94,77 +139,76 @@ export default class App extends React.Component {
   };
 
   render(){
-    const {series, nome, imagem_url, ano_lancamento, qtd_temp, editando, indexEditando} = this.state;
+    const {series, nomeSerie, imagem_urlSerie, ano_lancamentoSerie, qtd_tempSerie, editando, indexEditando} = this.state;
     return (
-      <div className="container">
         <main className="main">
-          <h1>Series</h1>
-          <hr/>
-          <h2>
-            {editando?`Editando:${series[indexEditando].nomeSerie}`:"Cadastrar uma nova serie"}
-          </h2>
-          <form onSubmit={this.onSubmit}>
-            <input placeholder = 'nome' value={nome} onChange={(e)=>{
-              this.setState({
-                nome: e.target.value
-              });
-            }}/>
-            <br></br>
-            <input placeholder = 'URL da imagem' value={imagem_url} onChange={(e)=>{
-              this.setState({
-                imagem_url: e.target.value
-              });
-            }}/>
-            <br></br>
-            <input placeholder = 'Ano de lançamento' value={ano_lancamento} onChange={(e)=>{
-              this.setState({
-                ano_lancamento: e.target.value
-              });
-            }}/>
-            <br></br>
-            <input placeholder = 'Quantidade de temporadas' value={qtd_temp} onChange={(e)=>{
-              this.setState({
-                qtd_temp: e.target.value
-              });
-            }}/>
-            <br></br>
-            <button typr='submit'>enviar</button>
-          </form>
-          <hr></hr>
+          <div className='Cabecalho'>
+            <div className='editando'>
+              <h2>
+                {editando?`Editando:${series[indexEditando]?.nome}`:"Cadastrar uma nova serie"}
+              </h2>
+              <form onSubmit={this.onSubmit}>
+                <input placeholder = 'nome' value={nomeSerie} onChange={(e)=>{
+                  this.setState({
+                    nomeSerie: e.target.value
+                  });
+                }}/>
+                <br></br>
+                <input placeholder = 'URL da imagem' value={imagem_urlSerie} onChange={(e)=>{
+                  this.setState({
+                    imagem_urlSerie: e.target.value
+                  });
+                }}/>
+                <br></br>
+                <input placeholder = 'Ano de lançamento' value={ano_lancamentoSerie} onChange={(e)=>{
+                  this.setState({
+                    ano_lancamentoSerie: e.target.value
+                  });
+                }}/>
+                <br></br>
+                <input placeholder = 'Quantidade de temporadas' value={qtd_tempSerie} onChange={(e)=>{
+                  this.setState({
+                    qtd_tempSerie: e.target.value
+                  });
+                }}/>
+                <br></br>
+                <button typr='submit'>enviar</button>
+              </form>
+            </div>
+            <hr/>
+          </div>
           <h1>Series</h1>
           <div className="conteudo">
            <main>
-             <div >
+             <div className='flex-container'>
                {series.map((s, index) => (
                  <div className="item">
                    <h2>Nome: {s.nome}</h2>
                    <img src={s.imagem_url} alt={s.nome}></img>
                    <h3>Ano de lançamento: {s.ano_lancamento}</h3>
                    <h3>Quantidade de temporadas: {s.qtd_temp}</h3>
-                   <button onClick={() => this.deletar(index)}>Deletar</button>
-                   <br></br>
-                   <button
-                   onClick={()=>{
-                     this.setState({
-                       editando: true,
-                       nomeSerie: s.nome,
-                       imagem_urlSerie: s.imagem_url,
-                       ano_lancamentoSerie: s.ano_lancamento,
-                       qtd_tempSerie: s.qtd_temp,
-                     });
-                   }}
-                   >Editar
-                   </button>
+                   <div className='botoes'>
+                    <button onClick={() => this.deletar(index)}>Deletar</button>
+                    <br/>
+                    <button
+                    onClick={()=>{
+                      this.setState({
+                        editando: true,
+                        nomeSerie: s.nome,
+                        imagem_urlSerie: s.imagem_url,
+                        ano_lancamentoSerie: s.ano_lancamento,
+                        qtd_tempSerie: s.qtd_temp,
+                      });
+                    }}
+                    >Editar
+                    </button>
+                   </div>
                 </div>
                ))}
              </div>
            </main>
          </div>
-
-        
         </main>
-
-      </div>
     )
   }
 
