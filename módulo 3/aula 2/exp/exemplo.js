520fe2f83ddbe388295a78f579dea2072ab28c2d
GabeDuarte <83724430+GabeDuarte@users.noreply.github.com>
2021-07-30 18:31:22 -0300

    .

--- módulo 3/aula 2/exp/exemplo.js
+++
@@ -1,9 +0,0 @@
-const soma = (numero1, numero2) => numero1 + numero2;
-
-const soma2 = (numero1, numero2) => {
-    return numero1 + numero2
-};
-
-console.log(soma(10,14));
-console.log(soma2(15, 44));
-
